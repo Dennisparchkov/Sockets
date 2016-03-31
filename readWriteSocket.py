9e30f3c887d5e51dc8a8ffc4238cf3ae05b9e2e4
Dennis Parchkov <dennis.parchkov@gmail.com>
2016-03-31 09:46:46 +0100

    Proxy used instead of middleware. Proxy takes arguments representing input and output sockets

--- readWriteSocket.py
+++ readWriteSocket.py
@@ -1,6 +1,6 @@
 '''Piece of code that reads input from a socket (arbitrary) and writes it to another socket
-3 tier architecture, this acts as the middleware, receiving some data from a socket, processing the data,
-then writing it to another socket(going to the front end database)
+proxy is used to receive data from one socket (client), do something with the data and write it to another socket
+for the server to read from
 '''
 
 
@@ -10,14 +10,14 @@ import struct
 
 
 INET_IP = '127.0.0.1' #done on localhost
-INPUT_PORT = 5001 #port used from input to middleware
-OUTPUT_PORT = 5002 #port used from middlware to server
-BUFFER_SIZE = 2
+INPUT_PORT = 5001 #port used for the receiving proxy socket
+OUTPUT_PORT = 5002 #port used for the receiving server socket
+BUFFER_SIZE = 64
 
 #sockets used
 inputSenderSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-middlewareReceiverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-middlewareOutputSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+proxyReceiverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+proxyOutputSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serverReceiverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 #boolean representing a successful reception of data from a process
@@ -31,47 +31,52 @@ def generateInputsocket():
     print "Client : " + str(inputSenderSocket.getsockname())
     inputSenderSocket.sendall(message)
     try:
-        data = inputSenderSocket.recv(BUFFER_SIZE)
+        responce = inputSenderSocket.recv(BUFFER_SIZE)
     except:
         print "Buffer size to small"
     inputSenderSocket.close()
 
 
 
-#Middle ware receives data (can do porcessing) and copies it to another socket (destined for the server)
-def middlewareReceiver():
+#Proxy receives data (can do processing) and copies it to another socket (destined for the server)
+#inputSocket = client socket that prxy reads input from,
+#outputSocket = socket used for output, writing the input received on this socket (for server)
+#
+def proxy(inputSocket, outputSocket):
 
-    middlewareReceiverSocket.bind((INET_IP, INPUT_PORT))
-    print "Proxy receiver : " + str(middlewareReceiverSocket.getsockname())
-    middlewareReceiverSocket.listen(5)
-    conn, addr = middlewareReceiverSocket.accept()
+    inputSocket.bind((INET_IP, INPUT_PORT))
+    print "Proxy receiver : " + str(inputSocket.getsockname())
+    inputSocket.listen(5)
+    connection, address = inputSocket.accept()
 
-    middlewareOutputSocket.connect((INET_IP, OUTPUT_PORT))
+    outputSocket.connect((INET_IP, OUTPUT_PORT))
 
     while 1:
         try:
-            data = conn.recv(BUFFER_SIZE)
+            data = connection.recv(BUFFER_SIZE)
             if not data: break
             print "Data received, Doing something with it"
             data = data + " And  i added this"
-            middlewareOutput(middlewareOutputSocket, data)
+            proxyOutput(outputSocket, data)
         except:
             print "Buffer size to small"
             break
-        conn.send(str(messageReceived))
+        connection.send(str(messageReceived))#send response
 
-    conn.close()
+    connection.close()
 
 
-#outputs middleware data to server socket
-def middlewareOutput(sock, data):
-      print "Proxy Sender: "+ str(sock.getsockname())
+#outputs proxy data to server socket
+#sock = second socket to write data on
+#data = input received from 1st socket(client)
+def proxyOutput(sock, data):
+      print "Proxy Sender: " + str(sock.getsockname())
       sock.send(data)
-      data = sock.recv(BUFFER_SIZE)
+      response = sock.recv(BUFFER_SIZE)
       sock.close()
 
 
-#Server reads data from middleware
+#Server reads data from proxy socket
 def server():
      serverReceiverSocket.bind((INET_IP, OUTPUT_PORT))
      print "Server: " + str(serverReceiverSocket.getsockname())
@@ -82,7 +87,7 @@ def server():
              data = serverConn.recv(BUFFER_SIZE)
              if not data: break
              print data
-             serverConn.send(str(messageReceived))
+             serverConn.send(str(messageReceived))#send response
          except:
             print "Buffer size to small"
             break
@@ -90,14 +95,12 @@ def server():
 
 
 #runs 3 separate threads representing 3 different processes:
-# middleware handles receiving message, procesing and adding to server socket
-# generation of input sockets with input, passing to middleware
-# server receiving message from middleware socket
+# proxy handles receiving input from client, processing and writing to server socket.
 def main():
      try :
-        Thread(target=middlewareReceiver, args=()).start()
-        Thread(target=generateInputsocket, args=()).start()
-        Thread(target=server, args=()).start()
+        Thread(target=proxy, args=(proxyReceiverSocket, proxyOutputSocket)).start()
+        Thread(target=generateInputsocket, args=()).start() #generates input data.
+        Thread(target=server, args=()).start() #echo server for reading from second socket.
      except:
          print "cannot start thereads"
 
